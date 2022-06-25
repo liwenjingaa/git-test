0f849f415aa0f69a2d1a3a981659a056e1ea3021
liwj9 <liwj9@yonyou.com>
2022-06-25 21:35:36 +0800

    9

--- src/nc/ufgov/test/Test.java
+++ src/nc/ufgov/test/Test.java
@@ -3,7 +3,7 @@ package nc.ufgov.test;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("添加信息111");
+        System.out.println("添加信息000");
         System.out.println("添加信息222");
         System.out.println("添加信息333");
         System.out.println("添加信息444");
