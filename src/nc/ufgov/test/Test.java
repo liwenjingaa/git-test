2d513e830849b6cc3148b3d58f8cfb9d0dde7c60
liwj9 <liwj9@yonyou.com>
2022-06-25 20:55:45 +0800

    1

--- src/nc/ufgov/test/Test.java
+++ src/nc/ufgov/test/Test.java
@@ -2,15 +2,6 @@ package nc.ufgov.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("aaaa");
-
-        System.out.println("hhhhahhahah");
-        System.out.println("hhhhahhahah");
-        System.out.println("hhhhahhahah");
-
-        System.out.println("2222222222");
-        System.out.println("333333333333333333");
-        System.out.println("666666666");
 
 
     }
