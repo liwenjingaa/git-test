daa62d4e85e2b26e42e4d3d6c996fd4c60fafd3a
liwj9 <liwj9@yonyou.com>
2022-06-25 19:50:29 +0800

    测试hot-fix

--- src/nc/ufgov/test/Test.java
+++ src/nc/ufgov/test/Test.java
@@ -4,8 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("aaaa");
 
-        System.out.println("bbbb");
-        System.out.println("aaa223");
-        System.out.println("cccc");
+        System.out.println("hhhhahhahah");
+
     }
 }
